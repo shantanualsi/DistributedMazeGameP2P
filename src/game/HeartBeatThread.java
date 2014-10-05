0a767baa84a67bd7600087e88c0ee5ffb89e8152
Shantanu Alshi <shantanuals@gmail.com>
2014-10-06 01:02:40 +0800

    Removed unncessary code

--- src/game/HeartBeatThread.java
+++ src/game/HeartBeatThread.java
@@ -40,23 +40,6 @@ public class HeartBeatThread extends Thread {
 			
 			
 		}
-		
-		/*try {
-			System.out.println("Starting heartbeat"); 
-			while(true){
-				//Send request to Backup
-				gs.handleHeartBeat();
-				System.out.println("-");
-				Thread.sleep(1000);
-				Thread.yield();
-			}
-		} catch (RemoteException re){
-			//Exception received => Backup is down.
-			System.out.println("Backup down! Now starting getting new backup");
-			gi.updateBackUpObject();
-		}catch (InterruptedException ie) {		
-			// TODO Auto-generated catch block
-			ie.printStackTrace();
-		}*/
+
 	}
 }
